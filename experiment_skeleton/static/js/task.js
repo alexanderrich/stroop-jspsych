933922f30fb20ac734374da076c1444f1dc21215
Alex Rich <alexandersrich@gmail.com>
2017-06-29 08:20:53 -0700

    set original task.js equal to task_0

--- experiment_skeleton/static/js/task.js
+++ experiment_skeleton/static/js/task.js
@@ -1,15 +1,28 @@
-/* load psiturk */
+/*
+ load psiturk:
+ - uniqueId, adServerLoc, and mode are parameters passed in to javascript by the psiTurk server.
+ */
 var psiturk = new PsiTurk(uniqueId, adServerLoc, mode);
 
+/* the timeline array is the top-level structure of the experiment */
 var timeline = [];
 
+/* create a simple text trial welcoming the participant */
 var welcome_block = {
     type: "text",
     text: "Welcome to the experiment. Press the SPACE key to begin."
+    /* challenge: set welcome_block to only end when the user presses SPACE*/
 };
 
+/* using the "push" method, we can add trials to the timeline as we go along */
 timeline.push(welcome_block);
 
+
+/*
+ initialize jsPsych:
+ - display_element is the id of the html element in which trials will display
+ - timeline is the array of trials to use
+ */
 jsPsych.init({
     display_element: 'jspsych-target',
     timeline: timeline
